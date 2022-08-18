1e9aa1c10282576b64b388970c718bb2716c6fe9
AAlfaroC <axel06alfaro@gmail>
2022-08-18 12:55:55 -0600

    Ready to go.

---
+++ day_2/variables.py
@@ -0,0 +1,29 @@
+# Day 2: 30 Days of python programming
+
+from pickle import FALSE, TRUE
+
+
+first_name = 'Axel'
+last_name = 'Alfaro'
+full_name = 'Axel Alfaro'
+country = 'Nicaragua'
+city = 'Managua'
+age = 29
+year = 200
+is_married = 'No'
+is_true = TRUE
+is_light_on = TRUE
+age_next_year, another_country = 30, 'Canada'
+
+type(first_name)
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
+type(age_next_year)
+type(another_country)
